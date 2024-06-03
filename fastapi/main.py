02539505352da635cb2dac8c2076adc485364757
Daniel Michell <daniel.michell@omnicommediagroup.com>
2024-06-03 12:21:11 +0100

    Formatting

--- fastapi/main.py
+++ fastapi/main.py
@@ -17,7 +17,9 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/myitem")  # Needs to be defined before a route of the same name with parameter. Otherwise will be read as
+@app.get(
+    "/items/myitem"
+)  # Needs to be defined before a route of the same name with parameter. Otherwise will be read as
 async def my_item():
     return {"item": "This is my item"}
 
@@ -51,7 +53,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 # If no parameters in URL, additional function arguments are treated as query parameters: http://127.0.0.1:8000/items/?skip=0&limit=10
 async def read_db(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/users/{user_id}/items/{item_id}")
