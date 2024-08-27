b0458a4a2dab6460c216a853dd57067b1d19db28
Daniel Michell <dan.michell1000@gmail.com>
2024-08-27 10:30:28 +0100

    c

--- c/main.c
+++ c/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+// Even though meow is defined after main, it is still recognized by the compiler by adding a function prototype before. A function prototype is a declaration of a function that tells the compiler about the number of arguments the function takes and the type of the return value. Essentially tells the compiler that it doesn't exist yet, but it will.
+void meow(int);
+
 int main(void)
 {
-    printf("Hello, World!\n");
+    meow(3);
+
     return 0;
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("Meow!\n");
+    }
+}
+
+int add(int x, int y)
+{
+    return x + y;
 }
